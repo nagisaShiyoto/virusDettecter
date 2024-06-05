920ee09ef66520a862b8a4f77a4417dde8b30c48
nagisa_shiyota <elon.gilad9@gmail.com>
2024-06-05 16:35:14 +0300

    added all the setup

---
+++ codeFile/source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+int main()
+{
+    std::string VirusPath="";
+    std::string filePAth="";
+    DIR* file=nullptr;
+    struct dirent *dp;
+
+
+
+    std::cin>>VirusPath;
+    std::cin>>filePAth;
+
+    
+    return 0;
+}
---
+++ dettector.cpp
@@ -0,0 +1,11 @@
+#include "dettector.h"
+
+dettector::dettector()
+{
+
+}
+
+dettector::~dettector()
+{
+
+}
